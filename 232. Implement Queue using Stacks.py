43087a665848e5aeda84ff836960f3153b193d9c
HEPHZIBAI <113971953+HEPHZIBAI@users.noreply.github.com>
2024-01-29 18:30:33 +0530

    Add files via upload

---
+++ 232. Implement Queue using Stacks.py
@@ -0,0 +1,74 @@
+'''
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, peek, and empty.
+All the calls to pop and peek are valid.
+ 
+
+Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.
+
+'''
+
+class MyQueue:
+    s=0
+    t=-1
+    h=[]
+    def __init__(self):
+        self.s=0
+        self.t=-1
+        self.h=[]        
+
+    def push(self, x: int) -> None:
+        self.t+=1
+        self.h.append(x)
+        #self.h[self.t]=x
+
+    def pop(self) -> int:
+        self.s+=1
+        return self.h[self.s-1]
+
+    def peek(self) -> int:
+        return(self.h[self.s])
+
+    def empty(self) -> bool:
+        return self.s-1==self.t
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
